31733b566b78337f94134447e9e89403603e5ab0
Michael T Smith <lionfishy@gmail.com>
2019-08-05 11:14:49 +0100

    Faster implementation

--- retrodetect/__init__.py
+++ retrodetect/__init__.py
@@ -47,8 +47,24 @@ def getshift(imgA,imgB,start=None,end=None,searchbox=100,step=8):
     shift = step*np.array(np.unravel_index(temp.argmax(), temp.shape))
     shift = shift - searchbox
     return shift
+    
+def ensemblegetshift(imgA,imgB,searchbox=100,step=8,searchblocksize=50,ensemblesizesqrt=3):
+    """
+    searchblock: how big each search image pair should be.
+    ensemblesizesqrt: number of items for ensemble for one dimension.
+    
+    """
+    starts = []
+    for x in np.linspace(0,imgA.shape[0],ensemblesizesqrt+2)[1:-1].astype(int):
+        for y in np.linspace(0,imgA.shape[1],ensemblesizesqrt+2)[1:-1].astype(int):
+            starts.append([x,y])
+            
+    shifts = np.zeros([len(starts),2])
+    for i,start in enumerate(starts):
+        shifts[i] = getshift(imgA,imgB,step=step,searchbox=searchbox,start=start,end=start+np.array([searchblocksize,searchblocksize]))
+    return np.median(shifts,0)    
 
-def getblockmaxedimage(img,blocksize=70,offset=2):
+def old_getblockmaxedimage(img,blocksize=70,offset=2):
     """
     Effectively replaces each pixel with approximately the maximum of all the
     pixels within offset*blocksize of the pixel.
@@ -83,6 +99,42 @@ def getblockmaxedimage(img,blocksize=70,offset=2):
     for x,blockx in enumerate(range(0,img.shape[0]-2*blocksize*offset,blocksize)):
         for y,blocky in enumerate(range(0,img.shape[1]-2*blocksize*offset,blocksize)):
             out_img[blockx+(blocksize*offset):blockx+blocksize+(blocksize*offset),blocky+(blocksize*offset):blocky+blocksize+(blocksize*offset)] = max_img[x,y]
+    return out_img
+    
+def getblockmaxedimagenew(img,blocksize, offset):
+    """
+    Effectively replaces each pixel with approximately the maximum of all the
+    pixels within offset*blocksize of the pixel (in a square).
+    
+    Get a new image of the same size, but filtered such that each square patch
+    of blocksize has its maximum calculated, then a search box of size
+    (1+offset*2)*blocksize centred on each pixel is applied which finds the
+    maximum of these patches.
+    
+    img = image to apply the filter to
+    blocksize = size of the squares
+    offset = how far from the pixel to look for maximum
+    """
+    k = int(arr.shape[0] / blocksize)
+    l = int(arr.shape[1] / blocksize)
+    maxes = arr.reshape(k,blocksize,l,blocksize).max(axis=(-1,-3)) #from https://stackoverflow.com/questions/18645013/windowed-maximum-in-numpy
+    
+    templist = []
+    xm,ym = maxes.shape
+    for xoff in range(-offset+1,offset,1): #(if offset=1, for xoff in [0]) (if offset=2, for xoff in [-1,0,1])...
+      for yoff in range(-offset+1,offset,1):
+        templist.append(maxes[xoff+offset:xoff+xm-offset,yoff+offset:yoff+ym-offset])
+    max_img = templist[0]
+    for im in templist[1:]:
+        max_img = np.maximum(max_img,im)
+    
+    out_img = np.empty_like(img)
+    new_inner_img = max_img.repeat(blocksize,axis=0).repeat(blocksize,axis=1)
+    out_img[blocksize*offset:-(blocksize*offset),blocksize*offset:-(blocksize*offset)] = new_inner_img
+    out_img[:blocksize*offset,:] = 255
+    out_img[-(blocksize*offset):,:] = 255
+    out_img[:,:blocksize*offset] = 255
+    out_img[:,-(blocksize*offset):] = 255
     return out_img
 
 def alignandsubtract(subimg,shift,foreimg,start=None,end=None):
