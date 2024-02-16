6a03647f4b23d14a079586e6115f97ebc2bc9657
Shi Min Chua <shiminc@gmail.com>
2024-02-16 17:28:16 +0000

    correct module name

--- src/bee_retrodetect/__init__.py
+++ src/bee_retrodetect/__init__.py
@@ -5,15 +5,6 @@ from importlib.metadata import version
 __version__ = version("bee-retrodetect")
 
 # populate package namespace
-from bee
-
--retrodetect.detect
-import detect, detectcontact
-from bee
-
--retrodetect.image_processing
-import ensemblegetshift, getblockmaxedimage, alignandsubtract
-from bee
-
--retrodetect.normxcorr2
-import normxcorr2
+from bee_retrodetect.detect import detect, detectcontact
+from bee_retrodetect.image_processing import ensemblegetshift, getblockmaxedimage, alignandsubtract
+from image_processing.normxcorr2 import normxcorr2
