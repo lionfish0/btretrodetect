b7d60482166112ca1b6293af51350f33c13407b4
Shi Min Chua <shiminc@gmail.com>
2024-03-15 13:06:15 +0000

    test contact dict

--- tests/test_detect.py
+++ tests/test_detect.py
@@ -59,9 +59,31 @@ def test_detectcontact(photo_list):
     contact, found, searchimg = detectcontact(photo_list,3)
     assert isinstance(contact, list)
     assert all(isinstance(item, dict) for item in contact)
+    assert len(contact) == 20
     assert found == False
     expected_searchimg = load((os.path.join(os.getcwd(), "tests", "data", "searchimg_3.npz")))
     expected_searchimg = expected_searchimg['arr_0']
     assert np.allclose(searchimg, expected_searchimg)
+
+    ## testing various key-value of one of the dict(5) in the contact list
+    assert contact[5]['x'] == 1750
+    assert contact[5]['y'] == 610
+
+    expected_contact_patch = load((os.path.join(os.getcwd(), "tests", "data", "contact_3_5_patch.npz")))
+    expected_contact_patch = expected_contact_patch['arr_0']
+    assert np.allclose(contact[5]['patch'], expected_contact_patch)
+
+    expected_contact_searchpatch = load((os.path.join(os.getcwd(), "tests", "data", "contact_3_5_searchpatch.npz")))
+    expected_contact_searchpatch = expected_contact_searchpatch['arr_0']
+    assert np.allclose(contact[5]['searchpatch'], expected_contact_searchpatch)
+
+    assert contact[5]['mean'] == 1
+    assert contact[5]['centre'] == 12
+    assert contact[5]['innersurround'] == 2
+    assert contact[5]['outersurround'] == 2
+    assert contact[5]['searchmax'] == 1
+    assert contact[5]['centremax'] == 4
+    assert contact[5]['confident'] == False
+    assert contact[5]['prediction'] == 51.251687502861024
 
 
